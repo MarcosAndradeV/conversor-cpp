50505872a0fcae6ea6c97136a344d8fe426280bc
MarcosAndradeV <mastermarcos1212@hotmail.com>
2025-02-25 23:31:05 -0300

    Initial

---
+++ src/conv.cpp
@@ -0,0 +1,63 @@
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <stack>
+#include <string>
+
+// #define DEBUG
+#ifdef DEBUG
+    #define dbg(item) \
+        (std::cout << __FILE__ << ":" << __LINE__ << " " << #item " = " << item << std::endl, item)
+#else
+    #define dbg(item) item
+#endif
+
+#define shift(xs_len, xs) (assert(xs_len > 0), xs_len--, *xs++)
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    string program_name = shift(argc, argv);
+
+    if (argc < 1) {
+        cerr << "ERROR: Arquivo não provido.\n";
+        cout << "INFO: Para usar o programa utilize:\n";
+        cout << "    `" << program_name << " <arquivo.txt>" << "`\n";
+        return -1;
+    }
+
+    string arquivo = shift(argc, argv);
+    ifstream Arquivo(arquivo);
+    if (!Arquivo.is_open()) {
+        cerr << "ERROR: Falha ao abrir o arquivo \"" << arquivo << "\".\n";
+        return -1;
+    }
+
+    string bits;
+
+    char bit;
+    while(Arquivo.get(bit)) {
+        if (bit == '0' || bit == '1') {
+            bits.push_back(bit);
+        }
+    }
+    Arquivo.close();
+
+    string msg = "";
+    for(size_t i = 0; i < bits.size(); i+=8) {
+        stack<unsigned char> buffer;
+        for(size_t j = 0; j < 8; j++) {
+            buffer.push(bits[i + j]);
+        }
+        unsigned char letter = 0;
+        for(unsigned char n = 1; !buffer.empty(); n++) {
+            if(buffer.top() == '1')
+                letter = (letter | (1 << (n - 1)));
+            buffer.pop();
+        }
+        msg.push_back(letter);
+    }
+
+    cout << "messagem: " << msg << endl;
+    return 0;
+    }
